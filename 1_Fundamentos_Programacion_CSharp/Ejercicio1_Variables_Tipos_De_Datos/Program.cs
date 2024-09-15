442c28b7be953be45973ca2f252ba1d248df0c17
KENNY JUAN REYES MENA <kr2021-2016@uce.edu.do>
2024-09-15 15:14:43 -0400

    Ejercicio 1 Terminado

--- 1_Fundamentos_Programacion_CSharp/Ejercicio1_Variables_Tipos_De_Datos/Program.cs
+++ 1_Fundamentos_Programacion_CSharp/Ejercicio1_Variables_Tipos_De_Datos/Program.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ejercicio1!");
+Console.WriteLine("");
+int num = 24;
+double flott = 4.5;
+string text = "Lola";
+Console.WriteLine(num);
+Console.WriteLine(flott);
+Console.WriteLine(text);
 
 
 
