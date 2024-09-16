631a66fd89a38311204e0a071ae40ced8c3891ff
KENNY JUAN REYES MENA <kr2021-2016@uce.edu.do>
2024-09-16 11:27:27 -0400

    Ejercicio3 completado

--- 2_Control_Flujo_Conversion_Tipos/Ejercicio3_Control_Flujo/Program.cs
+++ 2_Control_Flujo_Conversion_Tipos/Ejercicio3_Control_Flujo/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ejercicio3");
+
+int numero =  0;
+Console.WriteLine("Escribir numero");
+numero  = int.Parse(Console.ReadLine());
+if (numero % 2 == 0)
+{
+    Console.WriteLine("El Numero Es Par");
+}
+else
+{
+    Console.WriteLine("El Numero es Impar");
+
+}
