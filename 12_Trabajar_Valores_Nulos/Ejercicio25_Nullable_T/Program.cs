ff8b5aed48b881823a0d7805e1ef67a682f7e80a
KENNY JUAN REYES MENA <kr2021-2016@uce.edu.do>
2024-09-16 16:16:56 -0400

    Terminado

--- 12_Trabajar_Valores_Nulos/Ejercicio25_Nullable_T/Program.cs
+++ 12_Trabajar_Valores_Nulos/Ejercicio25_Nullable_T/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("ejercicio25!");
+
+Console.WriteLine("Ingrese la edad de la persona (o deje en blanco para nulo):");
+string entrada = Console.ReadLine();
+
+int? edad = null;
+if (!string.IsNullOrEmpty(entrada))
+{
+    int edadInt;
+    if (int.TryParse(entrada, out edadInt))
+    {
+        edad = edadInt;
+    }
+    else
+    {
+        Console.WriteLine("Entrada inválida. Por favor, ingrese un número entero.");
+    }
+}
+if (edad.HasValue)
+{
+    Console.WriteLine($"La edad ingresada es: {edad}");
+}
+else
+{
+    Console.WriteLine("No se ingresó una edad válida.");
+}
