b5e91b47f11722aac49587a2266ebf1649d5b3aa
KENNY JUAN REYES MENA <kr2021-2016@uce.edu.do>
2024-09-16 14:14:41 -0400

    listo

--- 2_Control_Flujo_Conversion_Tipos/Ejercicio4_Conversion_Tipos/Program.cs
+++ 2_Control_Flujo_Conversion_Tipos/Ejercicio4_Conversion_Tipos/Program.cs
@@ -8,3 +8,5 @@ Console.WriteLine("Convertido a decimal" +  numeroDecimal);
 
 int numeroconvertido = (int)numeroDecimal;
 Console.WriteLine("converido nuevamente a entero" + numeroconvertido);
+// realmente se usa cuando necesitas convertir un tipo de dato o otro de forma deliverada 
+// por que la convercion no es automatica o se puede perder informacion 
