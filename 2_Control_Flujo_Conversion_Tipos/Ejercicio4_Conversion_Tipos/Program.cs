06df743c9e70309a04c2f1e210ddf6f68d85e97a
KENNY JUAN REYES MENA <kr2021-2016@uce.edu.do>
2024-09-16 14:13:29 -0400

    terminado y comentario agregado

--- 2_Control_Flujo_Conversion_Tipos/Ejercicio4_Conversion_Tipos/Program.cs
+++ 2_Control_Flujo_Conversion_Tipos/Ejercicio4_Conversion_Tipos/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("ejercicio4");
+Console.WriteLine("Escribir Numero Entero");
+int numero  = int.Parse(Console.ReadLine());
+decimal numeroDecimal = (decimal)numero / 10;
+
+Console.WriteLine("Convertido a decimal" +  numeroDecimal);
+
+int numeroconvertido = (int)numeroDecimal;
+Console.WriteLine("converido nuevamente a entero" + numeroconvertido);
